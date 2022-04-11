460b582bd90d90a6aa2bee046f8d23491b0d36e0
omkar shelke <omkar1144@gmail.com>
2022-04-11 12:27:28 +0400

    4th Commit

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.static("public")); // it used to serve the css which is stored i
 /*------------------------Global Variable Declairation------------------------------------*/
 
 let newitems = ["item1", "item2", "item3"]; // array is need to store multiple new item 
+let Workitems = [];
 
 /*-------------------------Get Request---------------------*/
 
@@ -22,21 +23,39 @@ app.get("/", function(req, res) {
         year: 'numeric'
     }
     let day = today.toLocaleDateString("en-US", option);
-    // Aboce code is used go displaying date in augmented manner
+    // Above code is used go displaying date in augmented manner
 
 
-    res.render("list", { KindOfDay: day, newKindItem: newitems });
-    //rendering list.ejs  sending day and newitems variable to ejs
+    res.render("list", { ListTittle: day, newKindItem: newitems });
+    //rendering list.ejs  sending "day" and "newitems" variable to ejs
 })
 
 
 /*-------------------------Post Request---------------------*/
 app.post("/", function(req, res) {
+    console.log(req.body);
     let newItem = req.body.newInput;
     //newItem = new item added by the user. 
-    newitems.push(newItem);
-    res.redirect("/");
+    if (req.body.list === 'Work') {
+        // "list" is name of the button and "Work" is the dynamic value it is holding
+        Workitems.push(newItem);
+        res.redirect("/work");
+    } else {
+        newitems.push(newItem);
+        res.redirect("/");
+    }
+    // Since we have two seprate list which Work and home the above logic help us to add the newItem to the respective list
 
+})
+
+/*-------------------------Get request(/work)..................*/
+app.get("/work", function(req, res) {
+    res.render("list", { ListTittle: "Work List", newKindItem: Workitems });
+
+})
+
+app.get("/home", function(req, res) {
+    res.render("home");
 })
 
 /*-------------------------Port Declairation---------------------*/
